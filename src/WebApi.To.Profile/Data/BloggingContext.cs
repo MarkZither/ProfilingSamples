d5bb5504b8a8bd2c967a4f9fec4254261052c6cd
MarkZither <mark.burton@zither-it.co.uk>
2021-10-10 23:56:20 +0200

    Add bogus and some sample data

--- src/WebApi.To.Profile/Data/BloggingContext.cs
+++ src/WebApi.To.Profile/Data/BloggingContext.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Bogus;
+using Bogus.Extensions;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApi.To.Profile.Data
@@ -18,10 +21,21 @@ namespace WebApi.To.Profile.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Blog>().HasData(new Blog { BlogId = 1, Url = "http://sample.com" });
+            var ids = 1;
+            var blog = new Faker<Blog>()
+                .RuleFor(m => m.BlogId, f => ids++)
+                .RuleFor(m => m.Url, f => f.Internet.Url());
+            List<Blog> blogs = blog.GenerateBetween(100, 150);
+            modelBuilder.Entity<Blog>().HasData(blogs);
 
-            modelBuilder.Entity<Post>().HasData(
-                new Post { BlogId = 1, PostId = 1, Title = "First post", Content = "Test 1" });
+            var postids = 1;
+            var lorem = new Bogus.DataSets.Lorem("en");
+            var post = new Faker<Post>()
+                .RuleFor(m => m.PostId, f => postids++)
+                .RuleFor(m => m.Title, lorem.Slug())
+                .RuleFor(m => m.Content, lorem.Sentence(400, 300))
+                .RuleFor(m => m.BlogId, m => m.PickRandom(blogs).BlogId);
+            modelBuilder.Entity<Post>().HasData(post.GenerateBetween(1000, 1500));
         }
     }
 
